3b513fb489317e41e1aca067839cdb047b8defd9
dev-daeun <devdaeun@gmail.com>
2025-07-23 17:33:13 +0900

    void 타입 실습

---
+++ section2/src/void.ts
@@ -0,0 +1,25 @@
+/* void 타입 */
+/* 주로 리턴 값이 없는 함수의 리턴 타입 어노테이션 할 때 사용 */
+function func1(): string { // 리턴 타입을 string 으로 명시
+    return "hello"
+}
+
+function func2(): void {  // 리턴 타입을 void 으로 명시
+    console.log("hello");
+}
+
+/* void 타입 변수에 허용하는 값 : undefined, null (이렇게는 잘 안 쓸듯) */
+/* tsconfig.json 에 strictTypeChecks: false 지정하면 다른 타입의 값들도 저장 가능하긴 함  */
+let a: void;
+a = undefined;
+a = null;
+// a = 1 // Type 'number' is not assignable to type 'void'
+
+/* 어떤 함수의 리턴 타입이 null, undefined 이려면 null, undefined 를 직접 리턴해야 함 */
+function returnNull(): null {
+    return null;
+};
+
+function returnUndefined(): undefined {
+    return undefined;
+};
